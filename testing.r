209796ea11d453ef5defbcd4ced58ff4f2489618
mrharisk <mrharisk@gmail.com>
2021-05-29 00:25:08 -0700

    Data Types & Basic Operations. (Part-1)

--- testing.r
+++ testing.r
@@ -1,3 +1,15 @@
 # This is from Github
-print("Hello from Github")
-print("Today is my first lesson for R-Programming")
+# Hello from Github
+# Today is my first lesson
+
+# Data Types and Basic Operation(Part 1)
+a=5
+class(a)
+a=5L
+class(a)
+# L is used to show the number that it is integer
+# and L is always used in a right place of number
+# vector is a most basic object
+# which contains same classes of objects
+# but list is a exception which can use different classes of objects
+# Empty vector can be created with the vector() function
