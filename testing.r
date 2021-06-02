751d0021b4768b92d2fd081de5ec7bf77f241417
mrharisk <mrharisk@gmail.com>
2021-06-01 22:42:38 -0700

    Attributes and Evaluation

--- testing.r
+++ testing.r
@@ -2,14 +2,24 @@
 # Hello from Github
 # Today is my first lesson
 
-# Data Types and Basic Operation(Part 1)
+print("Data Types and Basic Operation(Part 1)")
 a=5
 class(a)
 a=5L
 class(a)
 # L is used to show the number that it is integer
 # and L is always used in a right place of number
+# because in R numbers are treated as numeric objects
 # vector is a most basic object
 # which contains same classes of objects
 # but list is a exception which can use different classes of objects
 # Empty vector can be created with the vector() function
+# NaN represents an undefined value ("not a number")
+
+# Attributes of an object can be accessed using the attributes() function
+attributes(a)
+a <- 8
+y<-3
+print(y)
+y <- 4
+y
